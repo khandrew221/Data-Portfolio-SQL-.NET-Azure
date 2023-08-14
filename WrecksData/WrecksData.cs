c8983399984d89b1dd819c8a8b08611b47223898
K Andrew <142013251+khandrew221@users.noreply.github.com>
2023-08-14 21:20:16 +0100

    Added beginning of WrecksData project. Early work in progress.

---
+++ WrecksData/WrecksData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data;
+
+using (StreamReader reader = new StreamReader("D:\\VS Projects\\AzureSQLTest\\WrecksData\\Wrecks.txt"))
+{
+    String line;
+
+    /*while ((line = reader.ReadLine()) != null)
+    {
+        Console.WriteLine(line);
+    }*/
+
+    // 
+    line = reader.ReadLine();
+
+    String[] headers = line.Split(new char[] { '\t' });
+
+    foreach (String val in headers) { Console.WriteLine(val); }
+
+    /*
+    for (int i = 0; i < 2; i++)
+    {
+        line = reader.ReadLine();
+
+        String[] strVals = line.Split(new char[] { '\t' });
+
+        foreach (String val in strVals) { Console.WriteLine(val); }
+        
+    }*/
+}
